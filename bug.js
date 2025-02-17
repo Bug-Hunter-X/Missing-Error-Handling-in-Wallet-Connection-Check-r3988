8f7980c118b747175eb809208e200183fb6dfe9a
BugHunter-X <askzenai@gmail.com>
2025-02-17 01:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+function checkIfWalletIsConnected() {
+  const { ethereum } = window;
+
+  if (!ethereum) {
+    console.log('Make sure you have Metamask installed!');
+    return;
+  }
+
+  console.log('Wallet is connected!');
+}
